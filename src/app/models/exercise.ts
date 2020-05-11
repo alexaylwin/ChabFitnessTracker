cfc893ad862ab509f8d614e44879e334b0d29529
Alex <AlexAylwin@gmail.com>
2020-05-11 08:14:47 -0400

    Adds additional exercises

--- src/app/models/exercise.ts
+++ src/app/models/exercise.ts
@@ -5,15 +5,22 @@ export interface Exercise {
 }
 
 export enum UNITS {
-  'm',
+  'min',
   'sec',
-  'rep'
+  'rep',
+  'km'
 }
 
 export const EXERCISE_LIST: Array<Exercise> = [
   { type: 'Pull-up', id: 1, unit: UNITS.rep },
-  { type: 'Walking', id: 2, unit: UNITS.m },
-  { type: 'Sit up' , id: 3, unit: UNITS.rep }
+  { type: 'Walking', id: 2, unit: UNITS.km },
+  { type: 'Sit-up' , id: 3, unit: UNITS.rep },
+  { type: 'Push-up', id: 4, unit: UNITS.rep },
+  { type: 'Squat', id: 5, unit: UNITS.rep },
+  { type: 'Plank', id: 6, unit: UNITS.sec },
+  { type: 'Biking', id: 7, unit: UNITS.km },
+  { type: 'Skipping', id: 8, unit: UNITS.sec },
+  { type: 'General', id: 9, unit: UNITS.min },
 ]
 
 export interface ExerciseRecord {
