94523ea5f4f60a21c1a3a26f9d86f47b38bb3810
Alex <AlexAylwin@gmail.com>
2020-05-14 08:46:39 -0400

    Fixes labels to use the exercise unit instead of just type

--- src/app/models/exercise.ts
+++ src/app/models/exercise.ts
@@ -12,22 +12,22 @@ export interface ExerciseRecord {
 }
 
 export enum UNITS {
-  'min',
-  'sec',
-  'rep',
-  'km'
+  min = 'min',
+  sec = 'sec',
+  rep = 'rep',
+  km = 'km'
 }
 
 export const EXERCISE_LIST: Array<Exercise> = [
-  { type: 'Pull-up (reps)', id: 1, unit: UNITS.rep, multiplier: 0.8 },
-  { type: 'Walking (km)', id: 2, unit: UNITS.km, multiplier: 10 },
-  { type: 'Sit-up (reps)' , id: 3, unit: UNITS.rep, multiplier: 0.1 },
-  { type: 'Push-up (reps)', id: 4, unit: UNITS.rep, multiplier: 0.4 },
-  { type: 'Squat (reps)', id: 5, unit: UNITS.rep, multiplier: 0.2 },
-  { type: 'Plank (sec)', id: 6, unit: UNITS.sec, multiplier: 0.067 },
-  { type: 'Biking (km)', id: 7, unit: UNITS.km, multiplier: 10 },
-  { type: 'Skipping (sec)', id: 8, unit: UNITS.sec, multiplier: 0.056 },
-  { type: 'General (min)', id: 9, unit: UNITS.min, multiplier: 0.833 },
+  { type: 'Pull-up', id: 1, unit: UNITS.rep, multiplier: 0.8 },
+  { type: 'Walking', id: 2, unit: UNITS.km, multiplier: 10 },
+  { type: 'Sit-up' , id: 3, unit: UNITS.rep, multiplier: 0.1 },
+  { type: 'Push-up', id: 4, unit: UNITS.rep, multiplier: 0.4 },
+  { type: 'Squat', id: 5, unit: UNITS.rep, multiplier: 0.2 },
+  { type: 'Plank', id: 6, unit: UNITS.sec, multiplier: 0.067 },
+  { type: 'Biking', id: 7, unit: UNITS.km, multiplier: 10 },
+  { type: 'Skipping', id: 8, unit: UNITS.sec, multiplier: 0.056 },
+  { type: 'General', id: 9, unit: UNITS.min, multiplier: 0.833 },
 ];
 
 export function CalculateExercisePoints(exercise: Exercise, count: number): number {
